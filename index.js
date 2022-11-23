a36b2ac783aba697e6c516fc2872abd88d655953
DANNJUGUNA <ndan30071@gmail.com>
2022-11-23 09:52:36 +0300

    lab completed

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function divide(){
+   let num1=2000;
+   let num2=100;
+ return num1/num2;
+}
+
+const square =()=>{
+    let x=2;
+    return x**2;
+}
+const add=()=>{
+    let a=3;
+    let b=4;
+    return a+b;
+}
